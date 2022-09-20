ba2d358e5773b44d3758f49b00b66549d5ac9c3e
Abhishek Kumar <dk9554654249@gmail.com>
2022-09-20 16:02:16 +0530

    Password

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Card from "./components/card/Card";
 
 function App() {
+
+  const handlecopy=(e)=>{
+    console.log("Clicked To Copy Password");
+    // let text = document.getElementById("current-password");
+    // console.log(e.target.value);
+    // text.select();
+    // text.setSelectionRange(0,50);
+    navigator.clipboard.writeText("Password");
+    window.alert("Text Copied To Clipboard");
+  }
+  const [oldpassword,newpassword]=useState("password")
+  const handleCallback=(childData)=>{
+    // newpassword=e.target.value;
+    // newpassword(oldpassword);
+    console.log(childData);
+  }
+
   return (
     <div className="main">
       <div className="body">
@@ -10,13 +27,13 @@ function App() {
             Password Generator
           </div>
           <div className="password">
-            <div className="new-password">
-            Jhsjhsjsj2782
+            <div className="new-password" id='current-password'>
+            {oldpassword}
             </div>
-            <div className="copy-icon">
+            <div className="copy-icon" onClick={(e)=>handlecopy(e)}>
             </div>
           </div>
-          <div className="card">{Card()}</div>
+          <div className="card"><Card parentCallback={(e)=>handleCallback(childData)}/></div>
 
       </div>
     </div>
--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,3 +1,4 @@
+import { click } from "@testing-library/user-event/dist/click";
 import React from "react";
 import { useState } from "react";
 import "./styles/card.css";
@@ -5,14 +6,83 @@ import "./styles/card.css";
 
 function Card(props){
     
-    var slider,output;
+    var check1=0,check2=0,check3=0,check4=0;
+    
     let [oldlength,setlength]=useState(8);
     const handleslider=(e)=>{
-        slider=e.target.value;
-        // console.log(e.target.value);
         setlength(e.target.value);
         // console.log(setlength);
     }
+    var characters="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789@#$"
+    var setpassword='';
+    function generatepassword(){
+        var password="";
+        let uptochar=65;
+        let startchar=0;
+
+        var characterslength=oldlength;
+        console.log(characterslength);
+        for(let i=0;i<characterslength;i++)
+        {
+            let index=Math.floor(Math.random()*uptochar);
+            // console.log(index);
+            password+=characters.charAt(startchar+index);
+        }
+        console.log(password);
+        setpassword+=password;
+    }
+
+    const handlegenerate=(e)=>{
+        console.log("Handle Generate Invoked");
+        generatepassword();
+        props.parentCallback(e.setpassword);
+        e.preventDefault();
+    }
+    const handleonclick1=('click',(e)=>{
+        if(check1===0)
+        {
+                check1=1;
+        }
+        else{
+            check1=0;
+        }
+        console.log(check1);
+        console.log("Include Uppercase Activated");
+    });
+    const handleonclick2=('click',(e)=>{
+        if(check2===0)
+        {
+                check2=1;
+        }
+        else{
+            check2=0;
+        }
+        console.log(check2);
+        console.log("Include LowerCase Activated");
+    });
+    const handleonclick3=('click',(e)=>{
+        if(check3===0)
+        {
+                check3=1;
+        }
+        else{
+            check3=0;
+        }
+        console.log(check3);
+        console.log("Include Numbers Activated");
+    });
+    const handleonclick4=('click',(e)=>{
+        if(check4===0)
+        {
+                check4=1;
+        }
+        else{
+            check4=0;
+        }
+        console.log(check4);
+        console.log("Include Special Characters Activated");
+    });
+    
     return(
         <div className="card">
             <div className="password-strength">
@@ -29,10 +99,10 @@ function Card(props){
                 </div>
             </div>
                 <div className="query">
-                    <input type="checkbox"/><span id="query-type">Include Uppercase Letters</span><br/>
-                    <input type="checkbox"/><span id="query-type">Include Lowercase Letters</span><br/>
-                    <input type="checkbox"/><span id="query-type">Include Numbers</span><br/>
-                    <input type="checkbox"/><span id="query-type">Include Special Characters</span>
+                    <input type="checkbox" id="query1"  onChange={(e)=>handleonclick1(e)}/><span id="query-type">Include Uppercase Letters</span><br/>
+                    <input type="checkbox"id="query2"onChange={(e)=>handleonclick2(e)}/><span id="query-type">Include Lowercase Letters</span><br/>
+                    <input type="checkbox"id="query3"onChange={(e)=>handleonclick3(e)}/><span id="query-type">Include Numbers</span><br/>
+                    <input type="checkbox"id="query4"onChange={(e)=>handleonclick4(e)}/><span id="query-type">Include Special Characters</span>
                 </div>
                 <div className="strength">
                     <div className="heading">
@@ -42,7 +112,7 @@ function Card(props){
                         MEDIUM
                     </div>
                 </div>
-                <button className="generate">Generate</button>
+                <button className="generate" onClick={(e)=>handlegenerate(e)}>Generate</button>
 
         </div>
     )
